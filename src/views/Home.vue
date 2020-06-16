29ef229b3182143000d0955154e8f249e5b67074
cdmngz <crlsdmngz3@gmail.com>
2020-06-16 12:07:12 +0200

    Funcionando prodcutos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <v-carousel cycle interval="3000">
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+    <v-parallax
+    src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+  ></v-parallax>
+    <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">Holiis</h1>
+        <h4 class="subheading">Trollis</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+    <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal d-flex justify-center">
+        <strong class="subheading">Para no perderte de nada</strong>
+      </v-card-title>
+
+
+      <v-card-title class="teal d-flex justify-center">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>Maskavo</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+        icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+      }
+    },
   }
-}
 </script>
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,5 +1,158 @@
 <template>
-  <div>
-    <h1>Productos</h1>
+<div class="mx-auto">
+  <v-navigation-drawer class="red lighten-5" app permanent fixed expand-on-hover>
+    <v-toolbar flat v-for="(item, name, index) in maskavo" :key="index" :class="item.color">
+      <v-list-item class="px-0" @click="$vuetify.goTo('#'+name, {offset: 55})">
+        <v-list-item-icon>
+          <v-img height="25" width="25" :src="item.icon" alt="https://www.flaticon.com/authors/good-ware"></v-img>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-subtitle>{{ name | capitalize }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-toolbar>
+  </v-navigation-drawer>
+
+  <div :class="[container.color, 'pa-2']" v-for="(container, name, index) in maskavo" :key="index">
+    
+    <v-row class="text-center my-n4">
+      <v-card-text class="font-italic purple--text text--lighten-4">{{ name | capitalize }}</v-card-text>
+    </v-row>
+
+    <v-row>
+      <v-card
+        class="mx-xs-0 my-xs-8 ma-sm-8"
+        :id="name"
+        v-for="(item, i) in container.productos" :key="i"
+        width="500"
+        
+      >
+        <v-row>
+          <v-col cols="5">
+            <v-img :src="require(`../assets/${item.img}`)"></v-img>
+          </v-col>
+          <v-col>
+            <v-card-title>{{item.title}}</v-card-title>
+            <v-card-subtitle>{{item.subtitle}}</v-card-subtitle>
+            <v-card-text class="d-flex align-center py-0">Piel mixta<v-rating :value="5" color="blue" readonly size="12" full-icon="mdi-circle"></v-rating></v-card-text>
+            <v-card-text class="d-flex align-center py-0">Piel grasa<v-rating :value="5" color="blue" readonly size="12" full-icon="mdi-circle"></v-rating></v-card-text>
+            <v-card-text>{{item.descsubtitle}}</v-card-text>
+          </v-col>
+        </v-row>
+            <v-divider class="mx-4"></v-divider>
+        <v-row cols="12">
+          <v-card-text>{{item.cantidad}}gr</v-card-text>
+          <v-card-text>$ {{item.precio}}</v-card-text>
+            <v-chip-group>
+              <v-chip color="pink lighten-1" dark v-for="ing in item.composicion" :key="ing">{{ing}}</v-chip>
+            </v-chip-group>
+        </v-row>
+      </v-card>        
+    </v-row>
+
   </div>
+</div>
 </template>
+
+<script>
+
+export default {
+  name: 'Productos',
+  data: () => ({
+    opcion: 'kjlkj',
+    maskavo: {
+      jabones: {
+        icon: 'https://image.flaticon.com/icons/svg/2911/2911565.svg',
+        color: 'red lighten-5',
+        productos: [
+          {
+            img: 'JadeRefrescante.jpeg',
+            title: 'Jade Refrescante',
+            subtitle: 'Jabón de menta y eucalipto',
+            desctitle: 'Para piel mixta y grasa:',
+            descsubtitle: 'Propiedades bactericidas y antisépticas, reduce la grasa. Es refrescante y efectivo en el tratamiento para la piel.',
+            cantidad: 20,
+            composicion: ['azucar', 'flores', 'muchosColores', 'cuatro'],
+            precio: 200,
+          },
+          {
+            img: 'BifazRutilado.jpeg',
+            title: 'Bifaz Rutilado',
+            subtitle: 'Jabón de menta y eucalipto',
+            desctitle: 'Para piel mixta y grasa:',
+            descsubtitle: 'Propiedades bactericidas y antisépticas, reduce la grasa. Es refrescante y efectivo en el tratamiento para la piel.',
+            cantidad: 20,
+            composicion: ['azucar', 'flores', 'muchosColores'],
+            precio: 200,
+          },
+        ]
+      },
+      shampoo: {
+        icon: 'https://image.flaticon.com/icons/svg/2917/2917236.svg',
+        color: 'pink lighten-5',
+        productos: [
+          {
+            img: 'CariciaJaspe.jpeg',
+            title: 'Caricia Jaspe',
+            subtitle: 'Jabón de menta y eucalipto',
+            desctitle: 'Para piel mixta y grasa:',
+            descsubtitle: 'Propiedades bactericidas y antisépticas, reduce la grasa. Es refrescante y efectivo en el tratamiento para la piel.',
+            cantidad: 20,
+            composicion: ['azucar', 'flores', 'muchosColores'],
+            precio: 200,
+          },
+          {
+            img: 'BifazRutilado.jpeg',
+            title: 'Bifaz Rutilado',
+            subtitle: 'Jabón de menta y eucalipto',
+            desctitle: 'Para piel mixta y grasa:',
+            descsubtitle: 'Propiedades bactericidas y antisépticas, reduce la grasa. Es refrescante y efectivo en el tratamiento para la piel.',
+            cantidad: 20,
+            composicion: ['azucar', 'flores', 'muchosColores'],
+            precio: 200,
+          },
+        ]
+      },
+      acondicionador: {
+        icon: 'https://image.flaticon.com/icons/svg/2431/2431467.svg',
+        color: 'purple lighten-5',
+        productos: [
+          {
+            img: 'CariciaJaspe.jpeg',
+            title: 'Caricia Jaspe',
+            subtitle: 'Jabón de menta y eucalipto',
+            desctitle: 'Para piel mixta y grasa:',
+            descsubtitle: 'Propiedades bactericidas y antisépticas, reduce la grasa. Es refrescante y efectivo en el tratamiento para la piel.',
+            cantidad: 20,
+            composicion: ['azucar', 'flores', 'muchosColores'],
+            precio: 200,
+          }
+        ]
+      },
+      bombas: {
+        icon: 'https://image.flaticon.com/icons/svg/2431/2431473.svg',
+        color: 'blue lighten-5',
+        productos: [
+          {
+            img: 'CariciaJaspe.jpeg',
+            title: 'Caricia Jaspe',
+            subtitle: 'Jabón de menta y eucalipto',
+            desctitle: 'Para piel mixta y grasa:',
+            descsubtitle: 'Propiedades bactericidas y antisépticas, reduce la grasa. Es refrescante y efectivo en el tratamiento para la piel.',
+            cantidad: 20,
+            composicion: ['azucar', 'flores', 'muchosColores'],
+            precio: 200,
+          }
+        ]
+      }
+    }
+  }),
+  filters: {
+    capitalize: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  }
+}
+</script>
